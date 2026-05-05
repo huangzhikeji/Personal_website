9f77d2b04596764b8705e6d6bad8273c3c7b3b90
huangzhi <huangzhikeji@huangzhikeji.serv00.net>
2026-05-06 04:34:33 +0800

    Update [[catchall]].js

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,8 +1,5 @@
-// functions/[[catchall]].js - 极简测试版
-export async function onRequest({ request, env }) {
-    // 返回一个最简单的成功消息
-    return new Response('✅ 边缘函数 (Edge Function) 工作正常！这是一个测试页面。', {
-        status: 200,
+export async function onRequest() {
+    return new Response('Hello World', {
         headers: { 'Content-Type': 'text/plain' }
     });
 }
