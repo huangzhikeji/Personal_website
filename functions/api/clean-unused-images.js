485858b937398c8ad1ffa91c55a7bde2b3ed11b8
huangzhi <huangzhikeji@huangzhikeji.serv00.net>
2026-05-06 13:34:52 +0800

    Update clean-unused-images.js

--- functions/api/clean-unused-images.js
+++ functions/api/clean-unused-images.js
@@ -3,11 +3,11 @@ export async function onRequest({ request, env }) {
     const match = cookie.match(/admin_token=([^;]+)/);
     let isLoggedIn = false;
     if (match) {
-        const session = await NAV_KV.get(`session:${match[1]}`);
+        const session = await NAV_KV.get('session:' + match[1]);
         isLoggedIn = session !== null;
     }
     if (!isLoggedIn) {
-        return new Response(JSON.stringify({ code: 401, message: '未登录' }), {
+        return new Response(JSON.stringify({ code: 401, message: 'Not logged in' }), {
             status: 401,
             headers: { 'Content-Type': 'application/json' }
         });
@@ -17,26 +17,28 @@ export async function onRequest({ request, env }) {
     const posts = data ? JSON.parse(data) : [];
     const referencedImages = new Set();
     
-    for (const post of posts) {
-        if (post.coverImage && post.coverImage.includes('/api/image/')) {
-            referencedImages.add(post.coverImage.split('/api/image/')[1]);
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        if (post.coverImage && post.coverImage.indexOf('/api/image/') >= 0) {
+            const filename = post.coverImage.split('/api/image/')[1];
+            referencedImages.add(filename);
         }
         if (post.content) {
-            const matches = post.content.match(/\/api\/image\/([^"'\s)]+)/g);
-            if (matches) {
-                for (const m of matches) {
-                    referencedImages.add(m.replace('/api/image/', ''));
-                }
+            const regex = /\/api\/image\/([^"'\s)]+)/g;
+            let m;
+            while ((m = regex.exec(post.content)) !== null) {
+                referencedImages.add(m[1]);
             }
         }
     }
     
-    const imageListData = await NAV_KV.get('image_urls');
-    const allImages = imageListData ? JSON.parse(imageListData) : [];
+    const imageListStr = await NAV_KV.get('image_urls');
+    const allImages = imageListStr ? JSON.parse(imageListStr) : [];
     let deletedCount = 0;
     const remainingImages = [];
     
-    for (const img of allImages) {
+    for (let i = 0; i < allImages.length; i++) {
+        const img = allImages[i];
         if (referencedImages.has(img.filename)) {
             remainingImages.push(img);
         } else {
@@ -51,11 +53,12 @@ export async function onRequest({ request, env }) {
     
     await NAV_KV.put('image_urls', JSON.stringify(remainingImages));
     
-    return new Response(JSON.stringify({
+    const result = JSON.stringify({
         code: 200,
         total: allImages.length,
         referenced: referencedImages.size,
         unused: allImages.length - referencedImages.size,
         deleted: deletedCount
-    }), { headers: { 'Content-Type': 'application/json' } });
+    });
+    return new Response(result, { headers: { 'Content-Type': 'application/json' } });
 }
